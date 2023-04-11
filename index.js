818452d0f99ca538697dade2d2818cb0711a750e
ekaterina <pandaloveshardcore@gmail.com>
2023-04-11 14:15:03 +0400

    Added regular expression

--- index.js
+++ index.js
@@ -26,14 +26,16 @@ const timerEl = document.querySelector('span');
 // который будет анимировать timerEl
 const createTimerAnimator = () => (seconds) => {
   const duration = seconds + 1;
+  let sec = 0;
   const timer = setInterval(() => {
       hours = parseInt(seconds / 3600, 10);
       minutes = parseInt((seconds - (hours * 3600)) / 60, 10);
-      seconds = parseInt(seconds % 60, 10);
+      sec = parseInt(seconds % 60, 10);
       hours = hours < 10 ? "0" + hours : hours;
       minutes = minutes < 10 ? "0" + minutes : minutes;
-      seconds = seconds < 10 ? "0" + seconds : seconds;
-      timerEl.textContent = hours + ":" + minutes + ":" + seconds;
+      sec = sec < 10 ? "0" + sec : sec;
+      console.log(hours,minutes,sec)
+      timerEl.textContent = hours + ":" + minutes + ":" + sec;
       seconds--
     }, 1000);
   setTimeout(() => {
@@ -46,11 +48,16 @@ const animateTimer = createTimerAnimator();
 inputEl.addEventListener('input', () => {
   // Очистите input так, чтобы в значении
   // оставались только числа
+  let inputVal = inputEl.value;
+  if (typeof inputVal === "string") {
+    let numbersOnly = inputVal.replace(/[^0-9]/g, "");
+    inputEl.value = numbersOnly;
+  }
 });
 
 buttonEl.addEventListener('click', () => {
   const seconds = Number(inputEl.value);
-
+  console.log(seconds)
   // createTimerAnimator(seconds);
   animateTimer(seconds)
   inputEl.value = '';
